53609e0fdb7cdbb55aa3085d370b3a42efce81a4
Egor Dorichev <egordorichev@gmail.com>
2017-08-26 08:40:34 +0300

    Update conf.lua

--- conf.lua
+++ conf.lua
@@ -1,25 +1,50 @@
-config = {}
-config.version = {}
-config.version.major = 0
-config.version.minor = 0.1
-config.version.name = "bunny"
-config.version.string =
-	"v." .. config.version.major .. "."
-	.. config.version.minor .. " "
-	.. config.version.name
-config.window = {}
-config.window.width = 588
-config.window.height = 384
-config.canvas = {}
-config.canvas.width = 196
-config.canvas.height = 128
-config.fps = 60
-config.font = {}
-config.font.file = "font.ttf"
-config.font.letters = "abcdefghijklmnopqr"
-	.. "stuvwxyz1"
-	.. "234567890!?[](){}.,;:<>+=%#^*~/\\|$"
-	.. "@&`\"'-_ "
+config = {
+	version = {
+		major = 0,
+		minor = 0.1,
+		name = "bunny",
+		string = "v.0.0.1 bunny"
+	},
+
+	window = {
+		width = 588,
+		height = 384
+	},
+
+	canvas = {
+		width = 196,
+		height = 128
+	},
+
+	font = {
+		file = "font.ttf",
+		letters = "abcdefghijklmnopqrstuvwxyz"
+		.. "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+		.. "1234567890!?[](){}.,;:<>+=%#^*~/\\|$"
+		.. "@&`\"'-_ "
+	},
+
+	fps = 60,
+
+	palette = {
+		{ 0, 0, 0, 255 },
+		{ 29, 43, 83, 255 },
+		{ 126, 37, 83, 255 },
+		{ 0, 135, 81, 255 },
+		{ 171, 82, 54, 255 },
+		{ 95, 87, 79, 255 },
+		{ 194, 195, 199, 255 },
+		{ 255, 241, 232, 255 },
+		{ 255, 0, 77, 255 },
+		{ 255, 163, 0, 255 },
+		{ 255, 240, 36, 255 },
+		{ 0, 231, 86, 255 },
+		{ 41, 173, 255, 255 },
+		{ 131, 118, 156, 255 },
+		{ 255, 119, 168, 255 },
+		{ 255, 204, 170, 255 }
+	}
+}
 
 function love.conf(t)
 	t.window.width = config.window.width
