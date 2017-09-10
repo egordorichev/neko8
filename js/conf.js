1ea0ab8128935ba6d627a1e9024ef82fe14008c8
egordorichev <egordorichev@gmail.com>
2017-09-10 16:33:54 +0300

    Messing with js

---
+++ js/conf.js
@@ -0,0 +1,18 @@
+var config = {
+	version : {
+		major : 0,
+		minor : 0.1,
+		name : "kitten",
+		string : "v0.0.3 kitten"
+	},
+
+	window : {
+		width : 576,
+		height : 384
+	},
+
+	canvas : {
+		width : 192,
+		height : 128
+	}
+}
